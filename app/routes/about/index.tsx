ec0a356d9d4222cb486abd41ee479bae0cfcf38e
Dawn Hajdu <dawn.hajdu@gmail.com>
2025-08-01 21:30:46 -1000

    React Router Actions

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,10 +1,42 @@
 const AboutPage = () => {
     return ( 
-        <>
-        <h2 className="text-3xl font-bold text-white mb-2">
-            Hey, I'm Brat! 👋
-        </h2>
-        </>
+        <div className="max-w-5xl mx-auto px-6 py-16 bg-gray-900">
+            {/* Intro */}
+            <div className="flex flex-col md:flex-row md:items-start items-center gap-10 mb-12">
+                <img src="/images/profile.jpg" alt="profile" className="w-40 h-40 rounded-full object-cover border-4 border-blue-500 shadow-md" />
+                <div>
+                     <h1 className="text-3xl font-bold text-white mb-2">
+                        Hey, I'm Brat! 👋
+                    </h1>
+                    <p className="text-gray-300 text-lg">
+                        I'm a passionate web developer and content creator who loves building fridendly digital experiences and helping others grow into confident, modern developer.
+                    </p>
+                </div>
+            </div>
+
+            {/* Tech Stach */}
+            <h2 className="text-2xl font-semibold text-white mb-4">
+                🚀 Tech I use
+            </h2>
+            <ul className="flex flex-wrap gap-4 text-sm text-gray-300">
+                {[
+                    'React',
+                    'Next.js',
+                    'Vue',
+                    'Tailwind CSS',
+                    'Node.js',
+                    'Laravel',
+                    'Prisma',
+                    'MongoDB',
+                    'PostgreSQL',
+                    'Appwrite',
+                    'Docker',
+                ].map((tech) => (
+                    <li key={tech} className="bg-gray-700 px-3 py-1 rounded-md">{tech}</li>
+                ))}
+            </ul>
+
+        </div>
      );
 }
  
--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,8 +1,55 @@
-const ContactPage = () => {
+import type { Route } from "./+types";
+import { Form } from "react-router";
+
+export async function action({ request }: Route.ActionArgs) {
+    const formData = await request.formData();
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const subject = formData.get('subject');
+    const message = formData.get('message');
+    const data = {
+        name,
+        email,
+        subject,
+        message,
+    };
+
+    return {message: 'Form submitted successfully', data};
+}
+
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
     return (  
-        <>
+        <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
             <h2 className="text-3xl font-bold text-white mb-8 text-center">📫 Contact Me</h2>
-        </>
+
+            { actionData?.message ? (
+                <p className="mb-6p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md">{actionData.message}</p>
+            ) : null}
+
+            <Form method="post" className="space-y-6">
+                <div>
+                    <label htmlFor="name" className="block text-sm font-medium text-gray-300">Full Name</label>
+                    <input type="text" name="name" id="name" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100" />
+                </div>
+
+                <div>
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-300">Email</label>
+                    <input type="email" name="email" id="email" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100" />
+                </div>
+
+                <div>
+                    <label htmlFor="subject" className="block text-sm font-medium text-gray-300">Subject</label>
+                    <input type="text" name="subject" id="subject" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100" />
+                </div>
+
+                <div>
+                    <label htmlFor="message" className="block text-sm font-medium text-gray-300">Message</label>
+                    <textarea name="message" id="message" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100" />
+                </div>
+
+                <button className="w-full bg-blue-600 text-white py-2 rounded-lg bg-blue-600 hover:bg-blue-700 cursor-pointer">Send Message</button>
+            </Form>
+        </div>
     );
 }
  
